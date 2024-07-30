a1e79d6bf5c5be29479900c50078286927e752f4
Bang Van <bangtranvan08@gmail.com>
2024-07-30 15:25:38 +0700

    Comment Feature

--- src/main/java/com/bangvan/apiblogapp/config/SecurityConfig.java
+++ src/main/java/com/bangvan/apiblogapp/config/SecurityConfig.java
@@ -7,19 +7,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -43,23 +40,29 @@ public class SecurityConfig {
         return configuration.getAuthenticationManager();
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         configure ->
                                 configure
+                                        .requestMatchers("/role/**").hasRole("ADMIN")
                                         .requestMatchers("/auth/**").permitAll()
-                                        .requestMatchers(HttpMethod.GET,"/user").hasRole("ADMIN")
-//                                        .requestMatchers(HttpMethod.DELETE,"/user/{id}").authenticated()
-//                                        .requestMatchers(HttpMethod.PUT,"/user/**").authenticated()
-//                                        .requestMatchers("/role/**").hasRole("ADMIN")
+                                        .requestMatchers(HttpMethod.GET,"/comment/**").permitAll()
+                                        .requestMatchers(HttpMethod.GET,"/post/**").permitAll()
+                                        .requestMatchers(HttpMethod.GET,"/category/**").permitAll()
+                                        .requestMatchers(HttpMethod.GET,"/user/{id}").permitAll()
+                                        .requestMatchers("/comment/**").authenticated()
+                                        .requestMatchers("/user/**").authenticated()
+                                        .requestMatchers("/category/**").authenticated()
                                         .anyRequest().authenticated()
-                        )
+                )
                 .exceptionHandling(exception->exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
--- src/main/java/com/bangvan/apiblogapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bangvan/apiblogapp/exception/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package com.bangvan.apiblogapp.exception;
 import com.bangvan.apiblogapp.dto.response.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -36,5 +39,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(apiResponse,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDetails> handleAccessDeniedException(AccessDeniedException exception,
+                                                                    WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.FORBIDDEN);
+    }
 
 }
